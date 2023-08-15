7bb45b847df495dba93e621c2a3dc37cc2e718e1
denisreys <112689124+denisreys@users.noreply.github.com>
2023-08-15 20:08:29 +0600

    release version

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+window.onload = function(){ 
+    //Автоматическая смена пункта меню
+    const navigationHeight = document.querySelector('.content').offsetTop;
+    const navigation = document.querySelectorAll('.navigation__item');
+    const content = document.querySelectorAll('.content__item');
+
+    window.addEventListener('scroll', () => {
+        let scrollMiddlePosition = window.scrollY + (window.innerHeight / 2);
+
+        content.forEach((el, i) => { 
+            if(el.offsetTop - navigationHeight <= scrollMiddlePosition){
+                navigation.forEach((el) => {
+                    if(el.classList.contains('navigation__item--active')){
+                        el.classList.remove('navigation__item--active');
+                    }
+                });
+
+                navigation[i].classList.add('navigation__item--active');
+            }
+        });
+    });
+};
+
