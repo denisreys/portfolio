dd2ce9d4c532e862496370c72a28f84553eb25a1
denisreys <112689124+denisreys@users.noreply.github.com>
2023-08-15 21:42:13 +0600

    fixed the bag

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,30 @@ window.onload = function(){
     const navigationHeight = document.querySelector('.content').offsetTop;
     const navigation = document.querySelectorAll('.navigation__item');
     const content = document.querySelectorAll('.content__item');
+    const lastContentKey = content.length - 1;
 
+    console.log(lastContentKey);
     window.addEventListener('scroll', () => {
-        let scrollMiddlePosition = window.scrollY + (window.innerHeight / 1.2);
-        
-        content.forEach((el, i) => { 
-            if(el.offsetTop - navigationHeight <= scrollMiddlePosition){
-                navigation.forEach((el) => {
-                    if(el.classList.contains('navigation__item--active')){
-                        el.classList.remove('navigation__item--active');
-                    }
-                });
+        let scrollMiddlePosition = window.scrollY + (window.innerHeight / 1.5);
 
-                navigation[i].classList.add('navigation__item--active');
+
+        function addActiveClass(key){
+            navigation.forEach((el) => {
+                if(el.classList.contains('navigation__item--active')){
+                    el.classList.remove('navigation__item--active');
+                }
+            });
+            navigation[key].classList.add('navigation__item--active');
+        }
+        content.forEach((el, i) => { 
+            if(window.scrollY == 0){
+                addActiveClass(0);
+            }
+            else if(window.scrollY + window.innerHeight == document.body.scrollHeight){
+                addActiveClass(lastContentKey);
+            }
+            else if(el.offsetTop - navigationHeight <= scrollMiddlePosition){
+                addActiveClass(i);
             }
         });
     });
